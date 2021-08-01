82972cbb21e549c8020fc18fca2dbe1f023297fe
KoGyeongtae <kokt0203@naver.com>
2021-08-01 18:27:36 +0900

    <EDIT> server setting

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/Backend-GoAtreugo-server/server/middleware"
 	"github.com/Backend-GoAtreugo-server/server/router"
 	"github.com/Backend-GoAtreugo-server/utils"
 	"github.com/savsgio/atreugo/v11"
@@ -11,8 +12,14 @@ func Start() {
 		Addr: "0.0.0.0:4030",
 	}
 	server := atreugo.New(config)
+	server.UseBefore(middleware.BeforeGlobal)
+
 	router.Start_v1(server)
 
+	server.GET("/", func(rc *atreugo.RequestCtx) error {
+		rc.HTTPResponse("root dir api test", 200)
+		return nil
+	})
 	err := server.ListenAndServe()
 	utils.HandleErr(err)
 }
