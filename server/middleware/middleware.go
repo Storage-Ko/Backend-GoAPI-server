a6f93e91712c7fd2fea5411cfe6717049e2ec8b9
KoGyeongtae <kokt0203@naver.com>
2021-08-02 03:15:42 +0900

    <ADD> jwt token middleware

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,8 +1,14 @@
 package middleware
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/Backend-GoAtreugo-server/utils"
 	"github.com/savsgio/atreugo/v11"
 	"github.com/savsgio/go-logger/v2"
+	"github.com/valyala/fasthttp"
 )
 
 func BeforeGlobal(ctx *atreugo.RequestCtx) error {
@@ -25,6 +31,38 @@ func BeforeView(ctx *atreugo.RequestCtx) error {
 
 func AfterView(ctx *atreugo.RequestCtx) error {
 	logger.Info("Middleware executed AFTER VIEW")
+
+	return ctx.Next()
+}
+
+func AuthMiddleware(ctx *atreugo.RequestCtx) error {
+	// Avoid middleware when you are going to login view
+	if string(ctx.Path()) == "/v1/login" {
+		return ctx.Next()
+	}
+
+	buffer := ctx.Request.Header.String()
+	slice := strings.Split(buffer, "Authorization: ")
+	jwt := strings.Split(slice[1], "\nAccept: */*")
+
+	jwtCookieStr := jwt[0]
+	jwtCookie1 := []byte(jwtCookieStr)
+	jwtCookie1 = jwtCookie1[:len(jwtCookie1)-1]
+	fmt.Printf("AuthorizationA: %v\n", jwtCookie1)
+
+	if len(jwtCookie1) == 0 {
+		return ctx.ErrorResponse(errors.New("login required"), fasthttp.StatusForbidden)
+	}
+
+	token, _, err := utils.ValidateToken(string(jwtCookie1))
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if !token.Valid {
+		return ctx.ErrorResponse(errors.New("your session is expired, login again please"), fasthttp.StatusForbidden)
+	}
 
 	return ctx.Next()
 }
