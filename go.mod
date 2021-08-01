f71e0c61b9625b5cbc5a07e5759ecd7c772af060
KoGyeongtae <kokt0203@naver.com>
2021-08-01 16:53:59 +0900

    <ADD> connect to DB

--- go.mod
+++ go.mod
@@ -10,6 +10,10 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/savsgio/atreugo v5.4.0+incompatible // indirect
 	github.com/savsgio/atreugo/v11 v11.8.0 // indirect
+	github.com/savsgio/go-logger v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/gorm v1.21.12 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 
+	"github.com/Backend-GoAtreugo-server/model"
 	"github.com/Backend-GoAtreugo-server/utils"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
+	"github.com/savsgio/atreugo/v11"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func main() {
 	var dbConfig map[string]string
 	dbConfig, err := godotenv.Read()
 	utils.HandleErr(err)
+
+	config := atreugo.Config{
+		Addr: "0.0.0.0:4030",
+	}
+	server := atreugo.New(config)
+	v1 := server.NewGroupPath("/v1")
+
+	v1.GET("/", func(ctx *atreugo.RequestCtx) error {
+		return ctx.TextResponse("Hello world")
+	})
 
 	mysqlCredentials := fmt.Sprintf(
 		"%s:%s@%s(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
@@ -24,9 +37,27 @@ func main() {
 		dbConfig["MYSQL_DBNAME"],
 	)
 
-	db, err := gorm.Open("mysql", mysqlCredentials)
-	defer db.Close()
+	db, err := gorm.Open(mysql.Open(mysqlCredentials), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	mysqlDB, err := db.DB()
+	utils.HandleErr(err)
+	db.AutoMigrate(&model.Admin{}, &model.User{}, &model.Comment{})
+	/*
+		admin := model.Admin{
+			Id:        "Admin",
+			Name:      "root",
+			Password:  "root",
+			CreatedAt: time.Now(),
+		}
 
+	*/
+	//db.Select("id", "name", "password").Create(&admin)
+
+	err = server.ListenAndServe()
 	utils.HandleErr(err)
 
+	defer mysqlDB.Close()
 }
