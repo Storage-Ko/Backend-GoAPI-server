54d27218ab7d8ff111799436b5295fcb21c894f2
KoGyeongtae <kokt0203@naver.com>
2021-08-01 23:52:12 +0900

    <ADD> Error Handling

---
+++ utils/error.go
@@ -0,0 +1,35 @@
+package utils
+
+import "github.com/savsgio/atreugo/v11"
+
+func BadRequestException(ctx *atreugo.RequestCtx) {
+	res := ErrorRes{
+		Status:  400,
+		Message: "Bad Request",
+	}
+	ctx.JSONResponse(res, res.Status)
+}
+
+func UnauthorizedException(ctx *atreugo.RequestCtx) {
+	res := ErrorRes{
+		Status:  401,
+		Message: "Unauthorized",
+	}
+	ctx.JSONResponse(res, res.Status)
+}
+
+func ForbiddenException(ctx *atreugo.RequestCtx) {
+	res := ErrorRes{
+		Status:  403,
+		Message: "Forbidden",
+	}
+	ctx.JSONResponse(res, res.Status)
+}
+
+func NotFoundException(ctx *atreugo.RequestCtx) {
+	res := ErrorRes{
+		Status:  404,
+		Message: "Not Found",
+	}
+	ctx.JSONResponse(res, res.Status)
+}
