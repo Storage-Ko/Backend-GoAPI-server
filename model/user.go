2d0bb890bafdd3a3dd2efea4815c7710a1680e30
KoGyeongtae <kokt0203@naver.com>
2021-08-04 00:43:53 +0900

    <EDIT> user model

--- model/user.go
+++ model/user.go
@@ -6,16 +6,10 @@ import (
 
 type User struct {
 	gorm.Model
-	Uid    uint   `gorm:"primaryKey;autoIncrement" json:"uid"`
-	Id     string `json:"id"`
-	Name   string `json:"name"`
-	Grade  int    `json:"grade"`
-	Status int    `json:"status"`
-}
-
-func FindById(id string) User {
-	//db :=
-	user := User{}
-	//db.Where(&User{Id: id}).First(&user)
-	return user
+	Uid      uint   `gorm:"primaryKey;autoIncrement" json:"uid"`
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Grade    int    `json:"grade"`
+	Status   int    `json:"status"`
+	Password string `json:"password"`
 }
