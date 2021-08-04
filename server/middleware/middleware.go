630abfd2ecbf13a8c53c7c74790f6aee5465fc5f
KoGyeongtae <kokt0203@naver.com>
2021-08-04 17:06:11 +0900

    <EDIT> Auth Middleware logic

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -4,35 +4,38 @@ import (
 	"net/http"
 
 	"github.com/Backend-GoAPI-server/utils"
+	"github.com/savsgio/go-logger/v2"
 )
-
-func JsonContentTypeMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Add("Content-Type", "application/json")
-		next.ServeHTTP(rw, r)
-	})
-}
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	// Avoid middleware when you are going to login view
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
 		if r.URL.Path == "/v1/login" {
 			next.ServeHTTP(rw, r)
+			return
 		}
 		if r.URL.Path == "/v1/signup" {
 			next.ServeHTTP(rw, r)
+			return
 		}
 
 		jwtCookie, err := utils.GetTokenString(rw, r)
 		utils.HandleErr(err)
 
-		token, _, err := utils.ValidateToken(string(jwtCookie))
-		utils.HandleErr(err)
-
-		if !token.Valid {
-			utils.ForbiddenException(rw)
+		token, user, err := utils.ValidateToken(string(jwtCookie))
+		if err != nil {
+			utils.UnauthorizedException(rw)
 			return
 		}
+
+		logger.Info(user.Username)
+
+		if !token.Valid {
+			utils.UnauthorizedException(rw)
+			return
+		}
+
 		next.ServeHTTP(rw, r)
 	})
 }
