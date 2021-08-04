87f816e33b171838b327e0242bf1721b08beebfa
KoGyeongtae <kokt0203@naver.com>
2021-08-05 01:35:32 +0900

    <ADD> comment

--- utils/utils.go
+++ utils/utils.go
@@ -10,35 +10,46 @@ import (
 	"github.com/savsgio/go-logger/v2"
 )
 
-func HandleCredential(err error) {
+// Handle critical error with throw panic
+func HandlePanic(err error) {
 	if err != nil {
 		logger.Error(err)
-		log.Panic(err)
+		log.Panic(err) // exit
 	}
 }
 
+// Handle error
 func HandleErr(err error) {
 	if err != nil {
 		logger.Error(err)
 	}
 }
 
+// Hash payload & return hash string
 func Hash(payload interface{}) string {
 	s := fmt.Sprintf("%v", payload)
 	hash := sha512.Sum512([]byte(s))
 	return fmt.Sprintf("%x", hash)
 }
 
+// Translate payload with []byte type to object
 func ByteToObj(payload []byte, object interface{}) {
+	// payload type translate
 	err := json.Unmarshal(payload, &object)
 	if err != nil {
 		logger.Error(err)
 	}
 }
 
+// Translate objest to byte & response payload with []byte type
 func MarshalAndRW(status int, res interface{}, rw http.ResponseWriter) {
+	// Set status code
 	rw.WriteHeader(status)
+
+	// Translate object to byte array
 	resByte, err := json.MarshalIndent(res, "", "	")
 	HandleErr(err)
+
+	// Response payload
 	rw.Write(resByte)
 }
