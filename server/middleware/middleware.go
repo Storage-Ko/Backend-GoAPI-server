2eec83a3da3625dc1f4dc9f6b4b323e204596341
KoGyeongtae <kokt0203@naver.com>
2021-08-05 01:24:07 +0900

    <ADD> comment

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -7,10 +7,13 @@ import (
 	"github.com/savsgio/go-logger/v2"
 )
 
+// Auth Middleware
 func AuthMiddleware(next http.Handler) http.Handler {
-	// Avoid middleware when you are going to login view
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// Response content type setting
 		rw.Header().Add("Content-Type", "application/json")
+
+		// Exception Path of validation
 		if r.URL.Path == "/v1/login" {
 			next.ServeHTTP(rw, r)
 			return
@@ -20,17 +23,21 @@ func AuthMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
+		// Get accessToken from request header
 		jwtCookie, err := utils.GetTokenString(rw, r)
 		utils.HandleErr(err)
 
+		// Accesstoken validation
 		token, user, err := utils.ValidateToken(string(jwtCookie))
 		if err != nil {
 			utils.UnauthorizedException(rw)
 			return
 		}
 
+		// User data validation -- incomplete
 		logger.Info(user.Username)
 
+		// Accesstoken valid field validation
 		if !token.Valid {
 			utils.UnauthorizedException(rw)
 			return
