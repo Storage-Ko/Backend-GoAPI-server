93147ed84a3a702767eaa4e39bb8b7ce70ad31db
KoGyeongtae <kokt0203@naver.com>
2021-08-05 20:43:52 +0900

    <EDIT> utils

--- utils/request.go
+++ utils/request.go
@@ -1,13 +1,19 @@
 package utils
 
+import "time"
+
 type LoginReq struct {
 	Id       string `json:"id"`
 	Password string `json:"password"`
 }
 
 type SignupReq struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Grade    int    `json:"grade"`
-	Password string `json:"password"`
+	Id       string    `json:"id"`
+	Provider string    `json:"provider"`
+	Name     string    `json:"name"`
+	Password string    `json:"password"`
+	Nickname string    `json:"nickname"`
+	Sex      string    `json:"sex"`
+	Birth    time.Time `json:"birth"`
+	Phone    int       `json:"phone"`
 }
