ff2b358573fe8f21882df794c21d016648e4e076
KoGyeongtae <kokt0203@naver.com>
2021-08-10 01:40:06 +0900

    <ADD> json key

--- model/model.go
+++ model/model.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	Uid      string    `gorm:"PRIMARY_KEY;Type:varchar(40)"`
-	Provider string    `gorm:"Type:varchar(30);default:'default'"`
-	Id       string    `gorm:"Type:varchar(30);NOT NULL"`
-	Name     string    `gorm:"Type:varchar(30);NOT NULL"`
-	Password string    `gorm:"Type:varchar(255);NOT NULL"`
-	Nickname string    `gorm:"Type:varchar(30);NOT NULL"`
-	Sex      string    `gorm:"Type:varchar(10);NOT NULL"`
-	Birth    time.Time `gorm:"Type:datetime;NOT NULL"`
-	Phone    int       `gorm:"Type:integer;NOT NULL"`
-	Date     time.Time `gorm:"Type:datetime;NOT NULL"`
+	Uid      string    `json:"uid" gorm:"PRIMARY_KEY;Type:varchar(40)"`
+	Provider string    `json:"provider" gorm:"Type:varchar(30);default:'default'"`
+	Id       string    `json:"id" gorm:"Type:varchar(30);NOT NULL"`
+	Name     string    `json:"name" gorm:"Type:varchar(30);NOT NULL"`
+	Password string    `json:"password" gorm:"Type:varchar(255);NOT NULL"`
+	Nickname string    `json:"nickname" gorm:"Type:varchar(30);NOT NULL"`
+	Sex      string    `json:"sex" gorm:"Type:varchar(10);NOT NULL"`
+	Birth    time.Time `json:"birth" gorm:"Type:datetime;NOT NULL"`
+	Phone    int       `json:"phone" gorm:"Type:integer;NOT NULL"`
+	Date     time.Time `json:"date" gorm:"Type:datetime;NOT NULL"`
 }
